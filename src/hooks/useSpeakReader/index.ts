16955efdd380be727fff03bcc732183729ef40da
fmruiz <x.francoruiz@gmail.com>
2024-05-02 14:51:52 -0300

    feat(hooks): add useScreenMessages and useSpeakReader

---
+++ src/hooks/useSpeakReader/index.ts
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+/**
+ * Custom hook that we can use to read the content of components by id.
+ * Or speak an specific message
+ */
+const useScreenReader = (): {
+    speakMessage: (message: string) => void;
+    setSpeechRate: (rate: number) => void;
+    speechRate: number;
+    speakComponentContent: (elementId: string) => void;
+} => {
+    /**
+     * State to save the velocity of voice
+     */
+    const [speechRate, setSpeechRate] = useState<number>(1.0);
+    /**
+     * Speech syntesis API
+     */
+    const speakMessage = (message: string) => {
+        const utterance = new SpeechSynthesisUtterance(message);
+        utterance.rate = speechRate;
+        speechSynthesis.speak(utterance);
+    };
+
+    const speakComponentContent = (elementId: string) => {
+        const element = document.getElementById(elementId);
+        if (element) {
+            const content = element.innerText;
+            speakMessage(content);
+        }
+    };
+
+    useEffect(() => {
+        /**
+         * Clear the announce when the component dismount
+         */
+        return () => {
+            speechSynthesis.cancel();
+        };
+    }, []);
+
+    return {
+        speakMessage,
+        setSpeechRate,
+        speechRate,
+        speakComponentContent,
+    } as const;
+};
+
+export default useScreenReader;
