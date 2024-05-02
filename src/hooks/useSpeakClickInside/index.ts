31c4e49c3e30dc88d4e7481da7f63a3836cb7c33
fmruiz <x.francoruiz@gmail.com>
2024-05-02 15:11:28 -0300

    feat(hooks): add useSpeakClickInside

---
+++ src/hooks/useSpeakClickInside/index.ts
@@ -0,0 +1,29 @@
+import { RefObject, useEffect } from 'react';
+/**
+ * Custom hook that detect if you click inside of an specific component.
+ *
+ * @param ref - Ref is the reference element
+ * @param message - An specific message to reproduce with voice
+ */
+const useSpeakClickInside = (
+    ref: RefObject<HTMLElement>,
+    message: string = 'INSIDE'
+) => {
+    const speakMessage = (message: string) => {
+        const utterance = new SpeechSynthesisUtterance(message);
+        speechSynthesis.speak(utterance);
+    };
+    useEffect(() => {
+        const handleDetectClickInside = (event: MouseEvent) => {
+            if (ref.current && ref.current.contains(event.target as Node)) {
+                speakMessage(message);
+            }
+        };
+        document.addEventListener('mousedown', handleDetectClickInside);
+        return () => {
+            document.removeEventListener('mousedown', handleDetectClickInside);
+        };
+    }, [ref, message]);
+};
+
+export default useSpeakClickInside;
