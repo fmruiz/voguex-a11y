f34aa2c7607a4c805c8f0dad9e7d76cdce52bfe6
fmruiz <x.francoruiz@gmail.com>
2024-05-02 22:58:30 -0300

    chore(app): clear app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import React from "react";
-import useSpeakAnnouncer from "./hooks/useSpeakAnnouncer";
 
 const App = () => {
-  const { setSomethingChange, setMessage } = useSpeakAnnouncer();
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setSomethingChange(false), setMessage("button first");
-        }}
-      >
-        1
-      </button>
-      <button
-        onClick={() => {
-          setSomethingChange(true), setMessage("button second");
-        }}
-      >
-        2
-      </button>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
