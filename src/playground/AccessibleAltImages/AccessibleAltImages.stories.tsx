ac4dd8a18b3682358ce3abdcd9a45e403f5f7645
fmruiz <x.francoruiz@gmail.com>
2024-05-05 13:11:20 -0300

    feat(storybook): modify AccessibleAltImages story

--- src/playground/AccessibleAltImages/AccessibleAltImages.stories.tsx
+++ src/playground/AccessibleAltImages/AccessibleAltImages.stories.tsx
@@ -1,17 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import useAccessibleAltImages from "@voguex/hooks/useAccessibleAltImages";
 import ExampleImage from "../../assets/example-1.jpg";
 
 const AccessibleAltImages = () => {
+  const [detectEvent, setDetectEvent] = useState(false);
+
   const images = [
-    { id: "accessible-image-1", alt: "Accessible Image 1", image: "" },
+    {
+      id: "accessible-image-1",
+      alt: `${detectEvent ? "Accessible Image one after event" : "Default Alt Image 1"}`,
+      image: "",
+    },
     {
       id: "accessible-image-2",
-      alt: "Accessible Image 2",
+      alt: `${detectEvent ? "Accessible Image two after event" : "Default Alt Image 2"}`,
       image: ExampleImage,
     },
-    { id: "accessible-image-3", alt: "Accessible Image 3", image: "" },
+    {
+      id: "accessible-image-3",
+      alt: `${detectEvent ? "Accessible Image three after event" : "Default Alt Image 3"}`,
+      image: "",
+    },
   ];
 
   const { updateAltImages } = useAccessibleAltImages();
@@ -21,7 +31,7 @@ const AccessibleAltImages = () => {
   }, [updateAltImages]);
 
   return (
-    <>
+    <section style={{ display: "flex", flexDirection: "column" }}>
       <p
         aria-label="title of the component"
         aria-describedby="say that we have a list of images bellow"
@@ -29,16 +39,27 @@ const AccessibleAltImages = () => {
       >
         List of images bellow to see the change in alt attribute:
       </p>
-      {images.map((image) => (
-        <img
-          key={image.id}
-          id={image.id}
-          src={image.image}
-          alt={"image.alt"}
-          width={200}
-        />
-      ))}
-    </>
+      <div
+        style={{ display: "flex", alignItems: "center", marginBottom: "15px" }}
+      >
+        {images.map((image) => (
+          <img
+            key={image.id}
+            id={image.id}
+            src={image.image}
+            alt={"image.alt"}
+            width={200}
+          />
+        ))}
+      </div>
+      <button
+        aria-label="button to update alt images"
+        style={{ width: "200px", margin: "0 auto" }}
+        onClick={() => setDetectEvent(!detectEvent)}
+      >
+        Update Alt attribute of images
+      </button>
+    </section>
   );
 };
 
