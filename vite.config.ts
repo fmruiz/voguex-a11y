ede6660fb46b027b32f96397b6e0a8b333c759bf
fmruiz <x.francoruiz@gmail.com>
2024-05-06 21:54:56 -0300

    chore(root): add some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,26 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts({ include: ["lib"] })],
   resolve: {
     alias: [{ find: "@voguex", replacement: resolve(__dirname, "./lib") }],
   },
   build: {
+    outDir: "dist",
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./lib/index.ts"),
+      },
+      external: ["react", "react/jsx-runtime"],
+    },
     lib: {
-      entry: resolve(__dirname, "lib/index.ts"),
+      entry: resolve(__dirname, "./lib/index.ts"),
       name: "voguex-a11y",
       fileName: "voguex-a11y",
     },
+    minify: "esbuild",
+    copyPublicDir: false,
   },
 });
