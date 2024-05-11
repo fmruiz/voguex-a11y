f4241d62db7ec77235e29803f012d1fc9976545b
fmruiz <x.francoruiz@gmail.com>
2024-05-11 16:26:30 -0300

    test(playground): add test case to AriaButton component

---
+++ lib/playground/AriaButton/AriaButton.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import { AriaButton } from "./AriaButton";
+
+describe("AccessibleAltImages", () => {
+  const { getByTestId } = render(<AriaButton />);
+
+  const btnComponent = getByTestId("button-aria");
+
+  test("should be add the aria attributes using useAriaButton", () => {
+    expect(btnComponent.getAttribute("aria-label")).toBe("Example Button");
+    expect(btnComponent.getAttribute("aria-pressed")).toBe("false");
+    expect(btnComponent.getAttribute("aria-disabled")).toBe("false");
+  });
+});
